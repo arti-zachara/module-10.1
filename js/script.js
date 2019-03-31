4089bf7440b47b75e9383ff229dba2eed6b00b6f
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-03-31 16:43:45 +0200

    Add progress bar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+// carousel
 var flickityCarousel = document.querySelector(".carousel");
 var flkty = new Flickity(flickityCarousel, {
   pageDots: false,
   hash: true
 });
 
+//reset button
 var resetButton = document.querySelector(".reset-button");
 resetButton.addEventListener("click", function() {
   flkty.select(0);
+});
+
+// progress bar
+var progressBar = document.querySelector(".progress-bar");
+flkty.on("scroll", function(progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + "%";
 });
