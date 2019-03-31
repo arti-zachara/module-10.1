911be65dec8fb457a76200a9fb5e786860ab552a
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-03-31 20:25:43 +0200

    Add mustache code to script.js, problem with Mustache installation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// carousel
+// mustache template-carousel-cells
+var templateCarouselCells = document.getElementById("template-carousel-cells")
+  .innerHTML;
+Mustache.parse(templateCarouselCells);
+
+var generatedCarouselCells = "";
+
+for (var i = 0; i < carouselCells.length; i++) {
+  console.log(carouselCells);
+  generatedCarouselCells += Mustache.render(
+    templateCarouselCells,
+    carouselCells[i]
+  );
+}
+
+// insert generated html code to carousel
 var flickityCarousel = document.querySelector(".carousel");
+
+flickityCarousel.insertAdjacentHTML("beforeend", generatedCarouselCells);
+
+// carousel
 var flkty = new Flickity(flickityCarousel, {
   pageDots: false,
   hash: true
